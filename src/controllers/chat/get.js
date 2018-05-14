2ca6a56e6e5207d5a0d99faba3f73268579f2489
Maulid Ihsan <maulid.ihsan@mail.ugm.ac.id>
2018-05-14 13:31:41 +0700

    chat feature [alpha]

--- src/controllers/chat/get.js
+++ src/controllers/chat/get.js
@@ -3,9 +3,7 @@ const ChatModel = require('../../models/ChatModel');
 module.exports = (req, res, next) => {
   if (req.query.username) {
     ChatModel.findOne({ owner: req.user.username, sender: req.query.username })
-      .then((chat) => {
-        return res.status(200).json({ success: true, status: 200, data: chat });
-      })
+      .then(chat => res.status(200).json({ success: true, status: 200, data: chat }))
       .catch(err => next(err));
   } else {
     ChatModel.find({ owner: req.user.username })
